1fe5ce184b8739fc683a03050c1c07325bae9ab5
꧁𓆩༺  ⒷⓄⒷⓊⓇ  ༻𓆪꧂ <jbobur005@gmail.com>
2026-05-14 10:59:53 +0500

    zo'r

--- src/components/AnimatedPage.tsx
+++ src/components/AnimatedPage.tsx
@@ -1,11 +1,9 @@
 import { motion } from "framer-motion";
 import { ReactNode } from "react";
-import ParticleBackground from "./ParticleBackground";
 
 export default function AnimatedPage({ children, className = "" }: { children: ReactNode, className?: string }) {
   return (
     <>
-      <ParticleBackground />
       <motion.div
         initial={{ opacity: 0, y: 20, filter: "blur(10px)" }}
         animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
@@ -18,3 +16,4 @@ export default function AnimatedPage({ children, className = "" }: { children: R
     </>
   );
 }
+
