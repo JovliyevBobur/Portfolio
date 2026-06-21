2ce9fdfae405abccdb9ab9353d244fa2a39da01f
꧁𓆩༺  ⒷⓄⒷⓊⓇ  ༻𓆪꧂ <jbobur005@gmail.com>
2026-06-21 14:12:16 +0500

    zo'r

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,6 @@ import { TypeAnimation } from "react-type-animation";
 import { Github, Linkedin, Mail, Instagram, Layout, Database, Smartphone, Code } from "lucide-react";
 import { Button } from "./ui/button";
 import { LiquidButton } from "./ui/LiquidButton";
-import { BadgeLanyard } from "./ui/BadgeLanyard";
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
 import Spline from '@splinetool/react-spline';
@@ -29,9 +28,9 @@ const Hero = () => {
          <Spline scene="https://prod.spline.design/FcZ66SFMX1YbF-0I/scene.splinecode" className="w-full h-full cursor-grab active:cursor-grabbing" />
       </div>
 
-      {/* 3D Badge Lanyard - Rightmost */}
+      {/* 3D Spline Robot - Rightmost */}
       <div className="hidden lg:flex absolute top-[10%] right-[-5%] xl:right-0 w-[500px] h-[600px] z-10 pointer-events-auto opacity-90 items-center justify-center">
-         <BadgeLanyard />
+         <Spline scene="https://prod.spline.design/FcZ66SFMX1YbF-0I/scene.splinecode" className="w-full h-full cursor-grab active:cursor-grabbing" style={{ transform: 'scaleX(-1)' }} />
       </div>
 
       <div className="container mx-auto px-4 z-10 relative">
